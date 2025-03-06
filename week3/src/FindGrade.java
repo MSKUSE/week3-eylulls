9d2b04d764dd7080f709d2f13678c251a0ef3ac2
elifsueylularslan@posta.mu.edu.tr <elifsueylularslan@posta.mu.edu.tr>
2025-03-06 12:36:38 +0300

    Week3 lecture is completed

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -1,22 +1,30 @@
+import java.util.Scanner;
+
 public class FindGrade{
 
     public static void main(String[] args){
 
-        //Get the score from the user as a file parameter
-        //Calculate the grade based on the score
-        //Print the grade to the console
-        // 90 - 100 -> A
-        // 80 - 89 -> B
-        // 70 - 79 -> C
-        // 60 - 69 -> D
-        // 0 - 59 -> F
 
-
-
-
-
+        int length_of_args = args.length;
+        if (length_of_args == 0){
+            System.out.println("you should give a score");
+        }
+        else{
+            int grade = Integer.parseInt(args[0]);
+            if(grade>= 90){
+                System.out.println("grade : A");
+            }   else if(grade >= 80 && grade <89){
+                System.out.println("grade : B");
+            }   else if(grade >= 70 && grade < 79){
+                System.out.println("grade : C");
+            }   else if(grade >= 60 && grade < 69) {
+                System.out.println("grade : D");
+            }else if(grade >= 50 && grade < 59){
+                    System.out.println("grade : F");
+            }else{
+                System.out.println("Invalid score");
+            }
+        }
     }
-
-
 
 }
